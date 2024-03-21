7337b4dd434226eb3458901119660af767eea796
EZzzKryak <manicynaleksandr@yandex.ru>
2024-03-21 16:11:39 +0300

    add hash router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ import { Headphone } from "../../mockData/types";
 import { ProductsContext } from "../../context/appContext";
 // @ts-expect-error-ignore
 import { headphones } from "../../mockData/products.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { YMaps } from '@pbe/react-yandex-maps';
 
 const App = () => {
@@ -18,7 +18,7 @@ const App = () => {
   }, [products]);
 
   return (
-    <BrowserRouter basename="/neoflex">
+    <HashRouter basename="/neoflex">
       <YMaps>
         <ProductsContext.Provider value={headphones}>
           <div className={cls.app}>
@@ -28,7 +28,7 @@ const App = () => {
           </div>
         </ProductsContext.Provider>
       </YMaps>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
